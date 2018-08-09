126585094915830e026c80565e8db25f69753aab
sandeeptile <sandeeptile@gmail.com>
2018-08-09 14:05:35 +0530

    CONNECTIVITY_SERVICE added

--- app/src/main/java/com/example/sandy/kotlin_telephony_services/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_telephony_services/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sandy.kotlin_telephony_services
 
 import android.content.Context
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.TelephonyManager
@@ -23,7 +24,16 @@ class MainActivity : AppCompatActivity() {
         list.add(tManager.simOperatorName)
         list.add(tManager.simCountryIso)
 
+
+        // CONNECTIVITY_SERVICE
+
+        var cManager=getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(cManager.activeNetworkInfo!=null)
+            list.add(cManager.activeNetworkInfo.isConnected.toString())
+        else
+            list.add("false")
         var myadapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,list)
-        lv1.adapter = myadapter
+        lv1.adapter=myadapter
     }
 }
