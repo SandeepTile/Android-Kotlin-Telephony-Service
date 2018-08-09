af9007ffc3e6f6b9bb2b2f3f293c4ead8cca76da
sandeeptile <sandeeptile@gmail.com>
2018-08-09 12:40:19 +0530

    first commit

---
+++ app/src/main/java/com/example/sandy/kotlin_telephony_services/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sandy.kotlin_telephony_services
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
